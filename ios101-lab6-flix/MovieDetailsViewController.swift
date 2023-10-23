1e8c3726adfc0f733efc017961dfecad040b7304
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-10-23 10:00:13 -0400

    Set up view and controller

---
+++ ios101-lab6-flix/MovieDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MovieDetailsViewController.swift
+//  ios101-lab6-flix
+//
+//  Created by Topu Saha on 10/23/23.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+   
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    @IBOutlet weak var movieDescription: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+   
+
+}
